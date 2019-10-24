2df984ce0c28cc50915b8e5126a7825be1b0d715
超Carry的高高 <2506547326@qq.com>
2019-10-24 21:06:10 +0800

    添加ajax
    Signed-off-by: 超Carry的高高 <2506547326@qq.com>

--- first-project/src/main.js
+++ first-project/src/main.js
@@ -1,13 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 全部引入vant
 import Vue from 'vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 import App from './App'
 import router from './router'
-import 'vant/lib/index.css'
-import {Tabbar, TabbarItem, NavBar, Icon, Search, Card, Panel, Image, Field, Divider, Dialog, Row, Col, Grid, GridItem, Sticky, Button, DropdownMenu, DropdownItem, Tag, GoodsAction, GoodsActionButton} from 'vant'
+import $ from 'jquery'
+import axios from 'axios'
 
-Vue.use(Tabbar).use(TabbarItem).use(NavBar).use(Icon).use(Search).use(Card).use(Panel).use(Image).use(Field).use(Divider).use(Row).use(Col)
-Vue.use(Grid).use(GridItem).use(Sticky).use(Button).use(DropdownMenu).use(DropdownItem).use(Dialog).use(Tag).use(GoodsAction).use(GoodsActionButton)
+Vue.prototype.$axios = axios
+
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
